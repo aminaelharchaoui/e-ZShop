19d04b22049a815256d5a4ff1596dacd199f9da8
amina elharchaoui <amina.elharchaoui11@gmail.com>
2023-07-17 19:25:37 +0200

    Initial commit

---
+++ src/main/java/ZShop/example/eZShop/EZShopApplication.java
@@ -0,0 +1,13 @@
+package ZShop.example.eZShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EZShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EZShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ZShop/example/eZShop/EZShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ZShop.example.eZShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EZShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
